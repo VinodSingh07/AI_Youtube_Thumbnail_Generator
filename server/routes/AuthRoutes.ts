a5267fb0daddd020eeb8c68893c814eb861af744
Vinod <vinodlatwal03@gmail.com>
2026-01-03 20:06:44 +0530

    Create user authentication

---
+++ server/routes/AuthRoutes.ts
@@ -0,0 +1,12 @@
+import { loginUser, logoutUser, registerUser, verifyUser } from "../controllers/AuthControllers.js";
+import protect from "../middlewares/auth.js";
+import express from "express";
+
+const AuthRouter = express.Router();
+
+AuthRouter.post('/register', registerUser)
+AuthRouter.post('/login', loginUser)
+AuthRouter.get('/verify',protect, verifyUser)
+AuthRouter.post('/logout',protect, logoutUser)
+
+export default AuthRouter
